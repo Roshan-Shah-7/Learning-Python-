6d85f70778655b8f78c7dc2c1ff9d5604f467216
Roshan-Shah-7 <roshanshah12113@gmail.com>
2024-09-03 21:26:58 +0545

    Day 1 of learning Python

---
+++ Day 1.py
@@ -0,0 +1,53 @@
+'''
+History of Python developed in 1980s by Guido Van Rossum
+First version(0.9.0) relased in 1991
+'''
+# Modules and pip
+
+# Modules : 
+# collection of code written by some one else
+
+
+# pip is a package manager which is used to install modules.
+
+############################################################################
+
+# Variable and Datatype
+
+# Practice set
+
+# Q1 adding
+# x = 10
+# y = 20
+# print( x + y)
+
+# Q2 finding and printing the remender
+# x = 34
+# y = 5
+# z = 53 % 5
+# print("The remainder is", z)
+
+# Q3 checking the type of varuable
+# x = input("Enter anything: ")
+# print("The type of enter variable is", type(x))
+
+#Q4 comparison
+# a = 34
+# b = 80
+# if b > a:
+#     print("b is greater than a")
+# else:
+#     print("b is not greater than a")
+
+# Q5 average
+# x = float(input("Enter the first value: "))
+# y = float(input("Enter the second value: "))
+
+# avg = (x + y) / 2
+# print("The average is", avg)
+
+##############################################################################
+
+# String
+
+

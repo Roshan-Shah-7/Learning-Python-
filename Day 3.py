a4af0250411204b592c051bc19fca0ff418fee7b
Roshan-Shah-7 <roshanshah12113@gmail.com>
2024-09-15 12:46:19 +0545

    Day 3 of learning Python

---
+++ Day 3.py
@@ -0,0 +1,36 @@
+# List
+# List is similar to array
+# we can store any data type.
+# list are mutable means data can easy change without creating new list
+
+# friends = ['apple', 'Roshan', 7, 7.77, True]
+
+# list are mutable
+# we can change the values
+
+# friends[0] = 'banana'
+
+# print(friends)
+
+# print(friends[1:4])
+
+l1 = [1,3,7,4,5,6,2]
+
+# Methods :
+# l1.append(3)
+# l1.sort()
+# l1.reverse()
+# l1.pop(3)
+# print(l1)
+
+
+
+
+# Tuples
+# tuples are like list but they are un-mutable
+# cannont change the value of tuple list
+
+# a = (1,2,3,4,5)
+# b = (1,) # to show this is tuple if we don't use comma then python think that it is integer
+# print(type(a))
+
